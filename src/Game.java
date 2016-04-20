eb5fff322c2454d734235de22e53dd524e0063da
Iceotty <josephrikmaguire@gmail.com>
2016-04-20 20:04:34 +0100

    Added Rooms
    So if you input a correct response, and then input a correct response
    again, you get "Type in a proper response". This is because the current
    node has changed and has (currently) no paths.
    
    Game now ends when you reach a node with no paths/edges
    Using rooms now instead of nodes. can return to previous room

--- src/Game.java
+++ src/Game.java
@@ -6,26 +6,36 @@ import java.util.Scanner;
  */
 public class Game {
     String currentNode;
-    HashMap<String,Node> nodes;
+    HashMap<String,Room> nodes;
     Scanner scanner;
     boolean running = false;
     public Game(){
         running = true;
         nodes=new HashMap<>();
-        Node firstNode = new Node("node.firstNode","Eat, Sleep or Die (no caps pls)");
-        nodes.put(firstNode.name,firstNode);
+        //1,2,3,4,5,6
+        makeRoom("room.firstRoom","The dankest of dank rooms").east("room.secondRoom").southEast("room.thirdRoom").south("room.fourthRoom");
+        makeRoom("room.secondRoom","Somewhat dank. Has rare pepes on the walls").west("room.firstRoom").south("room.fifthRoom").southEast("room.sixthRoom");
+        makeRoom("room.thirdRoom","No dank memes in this one :(").northEast("room.firstRoom");
+        makeRoom("room.fourthRoom","Its okay I guess").north("room.firstRoom").east("room.fifthRoom").southEast("room.seventhRoom").south("room.eighthRoom");
+        makeRoom("room.fifthRoom","Not very dank tbh").southWest("room.seventhRoom").south("room.ninthRoom").north("room.secondRoom");
+        makeRoom("room.sixthRoom","Dank. Really really dank.").northWest("room.secondRoom");
+        makeRoom("room.seventhRoom","Has a warm pit of lava").northEast("room.fifthRoom").northWest("room.fourthRoom");
 
-        makeNode("node.eatNode","Nom Nom");
-        firstNode.addPath("eat","node.eatNode");
+//        Node firstNode = new Node("node.firstNode","Eat, Sleep or Die (no caps pls)");
+//        nodes.put(firstNode.name,firstNode);
+//
+//        makeNode("node.eatNode","Nom Nom");
+//        firstNode.addPath("eat","node.eatNode");
+//
+//        makeNode("node.sleepNode","ZzzZzzz");
+//        firstNode.addPath("sleep","node.sleepNode");
+//
+//        makeNode("node.deadNode","Yu ded");
+//        firstNode.addPath("die","node.deadNode");
+//        nodes.get("node.eatNode").addPath("die","node.deadNode");
+//        scanner=new Scanner(System.in);
+//        currentNode=firstNode.name;
 
-        makeNode("node.sleepNode","ZzzZzzz");
-        firstNode.addPath("sleep","node.sleepNode");
-
-        makeNode("node.deadNode","Yu ded");
-        firstNode.addPath("die","node.deadNode");
-
-        scanner=new Scanner(System.in);
-        currentNode=firstNode.name;
     }
     public static void main(String ...args){
         Game game = new Game();
@@ -40,7 +50,7 @@ public class Game {
             node.print();
         }
         String input;
-        while (nextNode==null){
+        while (nextNode==null&&running){
             input=read();
             nextNode=node.decide(input);
             if (nextNode==null){
@@ -54,6 +64,9 @@ public class Game {
     }
     public void gameLoop(){
         while (running){
+            if (nodes.get(currentNode).paths.isEmpty()){
+                running=false;
+            }
             processNode(getCurrentNode());
         }
     }
@@ -61,8 +74,9 @@ public class Game {
         return getNode(currentNode);
     }
 
-    private void makeNode(String nodeName, String nodeText){
-        nodes.put(nodeName,new Node(nodeName,nodeText));
+    private Room makeRoom(String roomName, String nodeText){
+        nodes.put(roomName,new Room(roomName,nodeText));
+        return nodes.get(roomName);
     }
 }
 
---
+++ src/Room.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Joseph on 20/04/2016.
+ */
+public class Room extends Node {
+    public Room(String name, String text) {
+        super(name, text);
+    }
+    public Room north(String room){
+        addPath("north",room);
+        return this;
+    }
+    public Room east(String room){
+        addPath("east",room);
+        return this;
+    }
+    public Room south(String room){
+        addPath("south",room);
+        return this;
+    }
+    public Room west(String room){
+        addPath("west",room);
+        return this;
+    }
+    public Room northEast(String room){
+        addPath("northEast",room);
+        return this;
+    }
+    public Room northWest(String room){
+        addPath("northWest",room);
+        return this;
+    }
+    public Room southEast(String room){
+        addPath("southEast",room);
+        return this;
+    }
+    public Room southWest(String room){
+        addPath("southWest",room);
+        return this;
+    }
+}
