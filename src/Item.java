55b8c8d1f5fc9f733c1601c2b5800939457072ad
Iceotty <josephrikmaguire@gmail.com>
2016-05-04 20:06:15 +0100

    Started adding items
    also and inventory and a way to make items in Game.java

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Joseph on 04/05/2016.
+ */
+public class Item {
+    String name;
+    String text;
+    public Item(String name, String text){
+        this.name = name;
+        this.text = text;
+    }
+}
