477b6d99d3de41294c8dd853986b5818a6e92ddf
Iceotty <josephrikmaguire@gmail.com>
2016-05-11 20:07:29 +0100

    Added NPCs, currently Pepe
    Need to add armour now, so pepe has to roll higher than the armour class

--- src/Game.java
+++ src/Game.java
@@ -20,6 +20,9 @@ public class Game {
     public Game(){
         running = true;
         nodes=new HashMap<>();
+        NPCs=new HashMap<>();
+        items=new HashMap<>();
+        traps=new HashMap<>();
         makeRoom("room.firstRoom","The dankest of dank rooms").east("room.secondRoom").southEast("room.thirdRoom").south("room.fourthRoom");
         makeRoom("room.secondRoom","Somewhat dank. Has rare pepes on the walls").west("room.firstRoom").south("room.fifthRoom").southEast("room.sixthRoom");
         makeRoom("room.thirdRoom","Barry levels of Dank").northWest("room.firstRoom").isDeathRoom=true;
@@ -38,7 +41,7 @@ public class Game {
         makeTrap("trap.chainsawTrap","You get sliced in half by a chainsaw blade","A chainsaw blade swings towards you from a wall");
         addTrap("trap.chainsawTrap","room.thirdRoom");
         addTrap("trap.arrowTrap", "room.fifthRoom");
-        makeNPC("npc.enemy","room.ninthRoom");
+        makeNPC("npc.enemy","room.ninthRoom","You have awakened a slumbering pepe, it attacks you!","You got dank'd by pepe");
         currentRoom = nodes.get("room.firstRoom").name;
         scanner=new Scanner(System.in);
         pc  =  new PlayerCharacter();
@@ -66,11 +69,18 @@ public class Game {
             }
 
         }
+        if (nodes.get(currentRoom).npc!=null){
+            nodes.get(currentRoom).npc.printText();
+            if(roll(20,14,"Pepe")){
+                nodes.get(currentRoom).npc.printKillText();
+                isDead = true;
+            }
+        }
         if (nodes.get(nextRoom).trap!=null){
             traps.get(nodes.get(nextRoom).trap).printTrap();
             System.out.println("type roll to roll the outcome");
             if (read().toLowerCase().equals("roll")){
-                roll=roll(20,pc.agility);
+                roll=roll(20,pc.agility,"You");
                 if (roll){
                     System.out.println("You successfully dodged the trap");
                     currentRoom=nextRoom;
@@ -143,17 +153,19 @@ public class Game {
         items.put(name,new Item(name,text));
         return items.get(name);
     }
-    private NPC makeNPC(String name,String room){
-        NPCs.put(name, new NPC(nodes.get(room)));
+    private NPC makeNPC(String name,String room, String text, String killText){
+        NPCs.put(name, new NPC(nodes.get(room),text,killText));
+        nodes.get(room).npc = NPCs.get(name);
         return NPCs.get(name);
+
     }
     private void addItem(String item, String room){
         nodes.get(room).item = items.get(item);
     }
-    private boolean roll(int max, int min){
+    private boolean roll(int max, int min,String roller){
         random = new Random();
         int randInt = random.nextInt(max)+1;
-        System.out.println("You rolled a "+randInt);
+        System.out.println(roller+" rolled a "+randInt);
         if (randInt>=min){
             return true;
         }else{
--- src/NPC.java
+++ src/NPC.java
@@ -4,8 +4,14 @@
 public class NPC extends Character{
     boolean isAggressive;
     Room room;
-    public NPC(Room room){
+    String text;
+    String killText;
+    public NPC(Room room,String text,String killText){
         this.room = room;
+        this.text = text;
+        this.killText = killText;
         isAggressive = true;
     }
+    public void printText(){System.out.println(text);}
+    public void printKillText(){System.out.println(killText);}
 }
